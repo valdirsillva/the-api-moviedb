ffde7604f83f91909ca3bc0608fd430fa22016a1
Valdir Silva <valdirpiresba@gamil.com>
2021-09-06 21:46:59 -0300

    Pegando a lista de filmes em exibicao na api e apresentando na aplicacao

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,26 @@
 
 
-window.addEventListener('load', function () {  
-    getMoviePopular();
+
+
+window.addEventListener('DOMContentLoaded', function () {  
+    
+    let url = new URLSearchParams(window.location.search);
+    let sufix = url.get('page');
+    
+    // Converto p/ caixa miniscula
+    sufix = sufix.toLocaleLowerCase();
+
+    switch(sufix) {
+        case 'popular':
+            getMoviePopular();
+          break;
+        case 'nowplaying':
+            getMovieNowPlaing();
+          break;  
+
+    }
+
+
 
 
 });
@@ -20,30 +39,33 @@ function render(data) {
 }
 
 
-async function getApiKey() {
+async  function getApiKey() {
 
     const json = await fetch('auth.json');
     const { apy_key } = await json.json();
 
-    return apy_key;
+    return  apy_key;
 }
 
+
+
 async function getMoviePopular() {
+
     const key = await getApiKey();
     const dataMoviesPopular = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${key}`);
 
     const allMoviesPopulation = await dataMoviesPopular.json();
     allMoviesPopulation.results.map(movie => {
-        // console.log(movie.poster_path);      
         render(movie);
     })
 }
 
 async function getMovieNowPlaing() {
+    const key = await getApiKey();
+
     const dataMoviesNowPlaying = await  fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${key}`);
     const allMoviesNowPlaying = await dataMoviesNowPlaying.json();
     allMoviesNowPlaying.results.map(movie => {
-
          render(movie);
     });
 }
